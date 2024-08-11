19fc8dbfb13cfd5fc7a68c8197b332719833c355
Maurizio Cescon <maurizio.cescon@gmail.com>
2024-08-11 17:18:12 +0200

    chore: setup lazy loading

--- projects/host/src/app/app.routes.ts
+++ projects/host/src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Routes } from '@angular/router';
 import { MfLoaderComponent } from '@mc/integration/mf-loader';
 import { startsWith } from '@mc/integration/utils';
 
+import { mf1Routes } from '@mc/components/mf1';
+import { mf2Routes } from '@mc/components/mf2';
+import { mf3Routes } from '@mc/components/mf3';
+import { mf4Routes } from '@mc/components/mf4';
+
 export const mfRoutes: Routes = [
   {
     matcher: startsWith('mf1'),
@@ -39,6 +44,26 @@ export const mfRoutes: Routes = [
       inputs: { mf: { elementId: 'mf4', tag: 'mf4-v18', routing: true } },
       outputs: { valueChanged: (v: string) => console.log(`valueChanged called with ${v}`) },
     },
+  },
+  { path: '**', redirectTo: '/mf1' },
+];
+
+export const lzRoutes: Routes = [
+  {
+    path: 'mf1',
+    children: mf1Routes,
+  },
+  {
+    path: 'mf2',
+    children: mf2Routes,
+  },
+  {
+    path: 'mf3',
+    children: mf3Routes,
+  },
+  {
+    path: 'mf4',
+    children: mf4Routes,
   },
   { path: '**', redirectTo: '/mf1' },
 ];

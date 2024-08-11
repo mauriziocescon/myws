cb0537c7a24081e56248b4f4eb998a9966350cda
Maurizio Cescon <maurizio.cescon@gmail.com>
2024-08-11 15:00:57 +0200

    chore: minor changes

--- projects/mc/integration/mf-base/base.component.ts
+++ projects/mc/integration/mf-base/base.component.ts
@@ -3,11 +3,14 @@ import {
   Component,
   computed,
   EventEmitter,
+  inject,
   Input,
   OnChanges,
   Output,
   SimpleChanges,
 } from '@angular/core';
+
+import { MfRouterService } from '@mc/integration/mf-router';
 
 @Component({
   selector: 'app-generic',
@@ -19,11 +22,21 @@ import {
     <button (click)="onClick()">Output</button>`,
 })
 export class BaseComponent implements OnChanges {
+  private mfRouter = inject(MfRouterService);
+
   @Input({ required: true }) mf: { elementId: string, tag: string } = { elementId: '', tag: '' };
   @Input({ required: true }) desc: string = '';
   @Output() valueChanged = new EventEmitter<string>();
 
   status = computed(() => `${this.mf.tag} loaded!`);
+
+  ngOnInit(): void {
+    this.mfRouter.setup({ elId: this.mf.elementId });
+  }
+
+  ngOnDestroy(): void {
+    this.mfRouter.cleanup();
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['desc'] && !changes['desc'].isFirstChange()) {

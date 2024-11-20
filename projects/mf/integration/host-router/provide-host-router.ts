fdf0064f8a74253ea78f7ec7befd5b7f583b1802
Maurizio Cescon <maurizio.cescon@gmail.com>
2024-11-20 15:45:42 +0100

    chore: code cleanup

--- projects/mf/integration/host-router/provide-host-router.ts
+++ projects/mf/integration/host-router/provide-host-router.ts
@@ -1,24 +1,20 @@
-import { APP_INITIALIZER, EnvironmentProviders, inject, makeEnvironmentProviders } from '@angular/core';
+import { EnvironmentProviders, inject, makeEnvironmentProviders, provideAppInitializer } from '@angular/core';
 
 import { HostRouterService } from './host-router.service';
 
 export function provideHostRouter(): EnvironmentProviders {
   return makeEnvironmentProviders([
-    {
-      provide: APP_INITIALIZER,
-      useFactory: () => {
-        const hostRouter = inject(HostRouterService);
+    provideAppInitializer(() => {
+      const hostRouter = inject(HostRouterService);
 
-        return () => new Promise<void>(resolve => {
-          // attaching HostRouterService to the global scope
-          // so it can be used by Mf
-          const global = (globalThis as any);
-          global.__myws__ = {};
-          global.__myws__.HostRouterService = hostRouter;
-          resolve();
-        });
-      },
-      multi: true,
-    },
+      return new Promise<void>(resolve => {
+        // attaching HostRouterService to the global scope
+        // so it can be used by Mf
+        const global = (globalThis as any);
+        global.__myws__ = {};
+        global.__myws__.HostRouterService = hostRouter;
+        resolve();
+      });
+    }),
   ]);
 }
